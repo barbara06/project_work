0da7f95e2b6d727070ae43edf76b2f99f204a81e
Barbara <barbara.palazzo@cfp-futura.education>
2020-11-28 15:29:42 +0100

    28/11

---
+++ album/js/main.js
@@ -0,0 +1,38 @@
+fetch('https://raw.githubusercontent.com/barbara06/project_work/master/film.json')
+    .then(
+        function(response) {
+            if (response.status !== 200) {
+                alert("File non raggiungibile");
+                return;
+            }
+
+            // Examine the text in the response
+            response.json().then(function(data) {
+                creaLista(data.film);
+            });
+        }
+    )
+
+function creaLista(lista) {
+    for (let i = 0; i < lista.length; i++) {
+        let film = '<div class="col-md-4">' +
+                    '<div class="card mb-4 shadow-sm">' +
+                        '<img src="' + lista[i].locandina + '" class="card-img-top">' +
+                        '<div class="card-body">' +
+                            '<h5 class="card-title">' + lista[i].titolo + '</h5>' + 
+                            '<p class="card-text">' + lista[i].genere + '(' + lista[i].anno + ')</p>' +
+                            '<p class="card-text"><i class="fa fa-star fa-lg stella"></i> ' + lista[i].rating +
+                            '<button type="button" class="btn btn-sm btn-outline-secondary float-right">Scheda</button>' +
+                            '</p>' +
+                        '</div>' +
+                    '</div>' +
+                    '</div>';
+
+
+
+
+
+        $("#listaFilm").append(film);
+
+    }
+}
